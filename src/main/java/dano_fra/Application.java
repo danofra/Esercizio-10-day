7723495670ba142ee2470cce7571aa44db2a36da
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-03-15 13:09:35 +0100

    add second actions and search

--- src/main/java/dano_fra/Application.java
+++ src/main/java/dano_fra/Application.java
@@ -47,22 +47,75 @@ public class Application {
         int scelta = scanner.nextInt();
         switch (scelta) {
             case 1:
-                System.out.println("Inserire i dati del libro");
-                Libri libro = new Libri(scanner.nextInt(), scanner.next(), scanner.nextInt(), scanner.nextInt(), scanner.next(), scanner.next());
+                System.out.println("Inserire l' ISBN del libro");
+                int ISBN = scanner.nextInt();
+                System.out.println("Inserire il titolo del libro");
+                String titolo = scanner.next();
+                System.out.println("Inserire l'anno di pubblicazione del libro");
+                int annoDiPubblicazione = scanner.nextInt();
+                System.out.println("Inserire il numero di pagine del libro");
+                int numeroPagine = scanner.nextInt();
+                System.out.println("Inserire l'autore del libro");
+                String autore = scanner.next();
+                System.out.println("Inserire il genere del libro");
+                String genere = scanner.next();
+                Libri libro = new Libri(ISBN, titolo, annoDiPubblicazione, numeroPagine, autore, genere);
                 libriList.add(libro);
                 System.out.println("Libro aggiunto correttamente");
                 break;
             case 2:
                 System.out.println("Inserire i dati della rivista");
-                Riviste rivista = new Riviste(scanner.nextInt(), scanner.next(), scanner.nextInt(), scanner.nextInt(), periodicita.valueOf(scanner.next()));
-                rivisteList.add(rivista);
+                System.out.println("Inserire l' ISBN della rivista");
+                int ISBN2 = scanner.nextInt();
+                System.out.println("Inserire il titolo della rivista");
+                String titolo2 = scanner.next();
+                System.out.println("Inserire l'anno di pubblicazione della rivista");
+                int annoDiPubblicazione2 = scanner.nextInt();
+                System.out.println("Inserire il numero di pagine della rivista");
+                int numeroPagine2 = scanner.nextInt();
+                System.out.println("Inserire la periodicità della rivista");
+                System.out.println("1 - MENSILE");
+                System.out.println("2 - SEMESTRALE");
+                System.out.println("3 - SETTIMANALE");
+                periodicita mensile;
+                periodicita semestrale;
+                periodicita settimanale;
+                int sc = scanner.nextInt();
+                Riviste rivista;
+                switch (sc) {
+                    case 1:
+                        mensile = periodicita.MENSILE;
+                        rivista = new Riviste(ISBN2, titolo2, annoDiPubblicazione2, numeroPagine2, mensile);
+                        rivisteList.add(rivista);
+                        break;
+                    case 2:
+                        semestrale = periodicita.SEMESTRALE;
+                        rivista = new Riviste(ISBN2, titolo2, annoDiPubblicazione2, numeroPagine2, semestrale);
+                        rivisteList.add(rivista);
+                        break;
+                    case 3:
+                        settimanale = periodicita.SETTIMANALE;
+                        rivista = new Riviste(ISBN2, titolo2, annoDiPubblicazione2, numeroPagine2, settimanale);
+                        rivisteList.add(rivista);
+                        break;
+                    default:
+                        System.out.println("Scelta non valida");
+                        break;
+                }
                 System.out.println("Rivista aggiunta correttamente");
                 break;
             case 3:
                 System.out.println("Inserire l' ISBN del libro da visualizzare");
-                int ISBN = scanner.nextInt();
-                if (ISBN == l1.getISBN()) {
+                int ISBN1 = scanner.nextInt();
+                // Creando i libri in modo statico, il confronto è possibile solo con tre casi
+                if (ISBN1 == l1.getISBN()) {
                     System.out.println(l1);
+                }
+                if (ISBN1 == l2.getISBN()) {
+                    System.out.println(l2);
+                }
+                if (ISBN1 == l3.getISBN()) {
+                    System.out.println(l3);
                 }
                 break;
             default:
